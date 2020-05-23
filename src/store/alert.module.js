216bce14b38363137dd75d5e37e3ac2596bca94d
h3adHunter <juanmanuelbaez1994@gmail.com>
2020-05-23 02:57:32 -0300

    Agrega feedback en la parte superior de la pagina

---
+++ src/store/alert.module.js
@@ -0,0 +1,54 @@
+const state = {
+    type: null,
+    message: null,
+    show: null
+};
+
+const actions = {
+    success({ commit }, message) {
+        commit('success', message);
+    },
+    error({ commit }, message) {
+        commit('error', message);
+    },
+    warning({ commit }, message) {
+        commit('warning', message);
+    },
+    clear({ commit }) {
+        commit('clear');
+    }
+};
+
+const mutations = {
+    success(state, message) {
+			state.type = 'success'
+			state.message = message
+			state.show = true
+			state.dismissSecs = 3
+    },
+    error(state, message) {
+			state.type = 'danger'
+			state.message = message
+			state.show = true
+			state.dismissSecs = 5
+    },
+    warning(state, message) {
+			state.type = 'warning'
+			state.message = message
+			state.show = true
+			state.dismissSecs = 5
+    },
+    clear(state) {
+        state.type = null;
+        state.message = null;
+				state.show = false;
+				state.dismissSecs = 0
+    }
+};
+
+export const alert = {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+};
