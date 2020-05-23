3be2c6b9102826f84893f3980a60161e222d5b38
h3adHunter <juanmanuelbaez1994@gmail.com>
2020-05-23 05:22:18 -0300

    Refactor para Codacy

--- src/services/profile.service.js
+++ src/services/profile.service.js
@@ -1,5 +1,5 @@
 import { authHeader } from '../utils/helpers/auth-header'
-import { userService } from './user.service'
+import { handleResponse } from '../utils/helpers/response-handler'
 
 const API_URL = process.env.API_URL || process.env.VUE_APP_API_URL
 
@@ -25,22 +25,4 @@ function update(profile) {
     };
 
     return fetch(`${API_URL}/profiles/${profile.id}`, requestOptions).then(handleResponse);
-}
-
-function handleResponse(response) {
-  return response.text().then((text) => {
-    const data = text && JSON.parse(text)
-    if (!response.ok) {
-      if (response.status === 401) {
-        // auto logout if 401 response returned from api
-        userService.logout()
-        location.reload(true)
-      }
-
-      const error = (data && data.message) || response.statusText
-      return Promise.reject(error)
-    }
-
-    return data
-  })
 }
